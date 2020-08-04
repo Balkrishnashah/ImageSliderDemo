722821bf29d5bc7b6277b816cd06bdb44ede4fad
[Balkrishnashah] <[shahkrishna493@gmail.com]>
2020-08-04 18:43:19 +0530

    image slider images loaded from internet implementation complete

--- app/src/main/java/com/balkrishnashah/imagesliderdemo/MainActivity.java
+++ app/src/main/java/com/balkrishnashah/imagesliderdemo/MainActivity.java
@@ -2,11 +2,16 @@ package com.balkrishnashah.imagesliderdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.balkrishnashah.imagesliderdemo.slider.ImageSliderModel;
 import com.balkrishnashah.imagesliderdemo.slider.SliderAdapter;
+import com.bumptech.glide.Glide;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +19,20 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     SliderView sliderView;
-    List<ImageSliderModel> imageSliderModelList;
+//    List<ImageSliderModel> imageSliderModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);
-        imageSliderModelList = new ArrayList<>();
+//        imageSliderModelList = new ArrayList<>();
         sliderView = findViewById(R.id.imageSlider);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.DROP);
 
-        imageSliderModelList.add(new ImageSliderModel(R.drawable.eat));
-        imageSliderModelList.add(new ImageSliderModel(R.drawable.sleep));
-        imageSliderModelList.add(new ImageSliderModel(R.drawable.code));
-        imageSliderModelList.add(new ImageSliderModel(R.drawable.repeat));
-        sliderView.setSliderAdapter(new SliderAdapter(this, imageSliderModelList));
+//        imageSliderModelList.add(new ImageSliderModel(R.drawable.eat));
+//        imageSliderModelList.add(new ImageSliderModel(R.drawable.sleep));
+//        imageSliderModelList.add(new ImageSliderModel(R.drawable.code));
+//        imageSliderModelList.add(new ImageSliderModel(R.drawable.repeat));
+
+        sliderView.setSliderAdapter(new SliderAdapter(this));  // pass this for above list images "imageSliderModelList"
     }
 }
--- app/src/main/java/com/balkrishnashah/imagesliderdemo/slider/SliderAdapter.java
+++ app/src/main/java/com/balkrishnashah/imagesliderdemo/slider/SliderAdapter.java
@@ -1,13 +1,16 @@
 package com.balkrishnashah.imagesliderdemo.slider;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.balkrishnashah.imagesliderdemo.R;
+import com.bumptech.glide.Glide;
 import com.smarteist.autoimageslider.SliderViewAdapter;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -15,11 +18,13 @@ public class SliderAdapter extends SliderViewAdapter<SliderViewHolder> {
 
 
     Context mcontext;
-    List<ImageSliderModel> imageSliderModelList;
 
-    public SliderAdapter(Context mcontext, List<ImageSliderModel> imageSliderModelList) {
+//    List<ImageSliderModel> imageSliderModelList;
+
+    public SliderAdapter(Context mcontext) { // add this parameter for drawable list from mainActivity List<ImageSliderModel> imageSliderModelList
         this.mcontext = mcontext;
-        this.imageSliderModelList = imageSliderModelList;
+//        this.imageSliderModelList = imageSliderModelList;
+
     }
 
     @Override
@@ -31,21 +36,49 @@ public class SliderAdapter extends SliderViewAdapter<SliderViewHolder> {
 
     @Override
     public void onBindViewHolder(SliderViewHolder viewHolder, int position) {
+//        below line is for from drawable
+//        viewHolder.msliderImageView.setImageResource(imageSliderModelList.get(position).getImage());
+//        https://balkrishnashahportfolio.ml/images/code.png
+        Uri uri0, uri1,uri2, uri3;
+                uri0 = Uri.parse("https://www.elegantthemes.com/blog/wp-content/uploads/2018/04/Best-Code-and-Text-Editors.png");
 
-        viewHolder.msliderImageView.setImageResource(imageSliderModelList.get(position).getImage());
+
+        switch (position){
+            case 0:
+//                Picasso.get().load(uri0).into(viewHolder.msliderImageView);
+//                Picasso.get().load("https://balkrishnashahportfolio.ml/images/repeat.jpg").into(viewHolder.msliderImageView);
+                Glide.with(viewHolder.itemView).load("https://balkrishnashahportfolio.ml/images/repeat.jpg").into(viewHolder.msliderImageView);
+                break;
+            case 1:
+//                Picasso.get().load("https://balkrishnashahportfolio.ml/images/code.png").into(viewHolder.msliderImageView);
+                Glide.with(viewHolder.itemView).load("https://balkrishnashahportfolio.ml/images/code.png").into(viewHolder.msliderImageView);
+                break;
+            case 2:
+//                Picasso.get().load("https://balkrishnashahportfolio.ml/images/sleep.png").into(viewHolder.msliderImageView);
+                Glide.with(viewHolder.itemView).load("https://balkrishnashahportfolio.ml/images/sleep.png").into(viewHolder.msliderImageView);
+                break;
+            case 3:
+//                Picasso.get().load("https://balkrishnashahportfolio.ml/images/eat.png").into(viewHolder.msliderImageView);
+                Glide.with(viewHolder.itemView).load("https://balkrishnashahportfolio.ml/images/eat.png").into(viewHolder.msliderImageView);
+                break;
+
+
+        }
+
 
     }
 
     @Override
     public int getCount() {
-        return imageSliderModelList.size();
+        return 4;
     }
 }
 class SliderViewHolder extends SliderViewAdapter.ViewHolder{
     ImageView msliderImageView;
-
+    View itemView;
     public SliderViewHolder(View itemView) {
         super(itemView);
+        this.itemView = itemView;
         msliderImageView = itemView.findViewById(R.id.sliderItemImageView_ID);
     }
 
